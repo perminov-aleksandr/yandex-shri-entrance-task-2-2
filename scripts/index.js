6558bd49cd41144ac8a79ddafde7b05d19f3600a
perminov.a <perminov.a@mail.ru>
2019-04-16 18:21:59 +0300

    add build task remove swiper

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,9 @@
-import Swiper from 'swiper';
 import MicroModal from 'micromodal';
 import FavouriteDevices from './favourite-devices.js';
 import Header from './header.js';
 import SliderControl from './slider-control.js';
 import KnobControl from './knob-control.js';
 import Animations from './animations.js';
-
-const scriptsSwiper = new Swiper('.favourite-scripts', {    
-    slidesPerView: 'auto', 
-    navigation: {
-        nextEl: '.favourite-scripts .paging__btn-next',
-        prevEl: '.favourite-scripts .paging__btn-prev',
-    }
-});
-
-const summaryContentSwiper = new Swiper('.summary__content__queue', {
-    //slidesPerView: 'auto', 
-    direction: 'vertical',
-    navigation: {
-        nextEl: '.summary__content__queue .paging__btn-next',
-        prevEl: '.summary__content__queue .paging__btn-prev',
-    }
-});
 
 FavouriteDevices.init();
 
